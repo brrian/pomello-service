99e2d8e5e0c652d386e73ee24fd6e93012857dba
Brian Wang <brrian@gmail.com>
2022-02-27 20:53:02 -0600

    feat: add overtime end handling

--- src/__tests__/pomelloService.overtime.spec.ts
+++ src/__tests__/pomelloService.overtime.spec.ts
@@ -23,4 +23,43 @@ describe('Pomello Service - Overtime', () => {
       type: 'TASK',
     });
   });
+
+  it('should not enter overtime if the next timer starts within overtimeDelay', () => {
+    const { advanceTimer, service } = mountPomelloService({
+      settings: {
+        overtimeDelay: 10,
+        set: ['task', 'shortBreak'],
+        shortBreakTime: 20,
+      },
+    });
+
+    service.selectTask('TASK_ID');
+    service.startTimer();
+    advanceTimer();
+    advanceTimer(9);
+
+    expect(service.getState().overtime).toBe(null);
+
+    service.continueTask();
+    advanceTimer(10);
+
+    expect(service.getState().overtime).toBe(null);
+  });
+
+  it('should end overtime when the next timer starts', () => {
+    const { advanceTimer, service } = mountPomelloService({
+      settings: {
+        set: ['task', 'shortBreak'],
+        overtimeDelay: 10,
+      },
+    });
+
+    service.selectTask('TASK_ID');
+    service.startTimer();
+    advanceTimer();
+    advanceTimer(15);
+    service.continueTask();
+
+    expect(service.getState().overtime).toBe(null);
+  });
 });
